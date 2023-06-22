235ad4f404c557e8875a090a2d23747f698f889d
Paul Czarnecki JR <120606767+pjczar@users.noreply.github.com>
2023-06-22 17:30:28 -0300

    trying to fix the disconnect

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+  process.env.REACT_APP_API_BASE_URL || "postgres://bssrhkpb:iUbXe6TBOn0MvuhrN4Wv24nLOUsLEogq@babar.db.elephantsql.com/bssrhkpb";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
