49423749c52fd55e73f4828e12b1c2a4dcc6674a
Paul Czarnecki JR <120606767+pjczar@users.noreply.github.com>
2023-06-23 17:14:53 -0300

    update to fix POST error

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -7,6 +7,14 @@
 const router = require("express").Router();
 const methodNotAllowed = require("../errors/methodNotAllowed");
 const controller = require("./reservations.controller");
+const cors = require("cors");
+
+router.use(cors());
+
+
+router.route("/new")
+    .post(controller.create)
+    .all(methodNotAllowed);
 
 router.route("/")
     .get(controller.list)
@@ -23,3 +31,14 @@ router.route('/:reservation_id')
     .all(methodNotAllowed);
 
 module.exports = router;
+
+/**
+*Here's a breakdown of the routes:
+*
+*GET /reservations: This route calls the *controller.list function to retrieve a list of *reservations.
+*POST /reservations: This route calls the *controller.create function to create a new *reservation.
+*PUT /reservations/:reservation_id/status: This route *calls the controller.updateStatus function to update *the status of a specific reservation.
+*GET /reservations/:reservation_id: This route calls *the controller.read function to retrieve a specific *reservation.
+*PUT /reservations/:reservation_id: This route calls *the controller.update function to update a specific *reservation.
+*The methodNotAllowed function is used as a catch-all *for any other HTTP methods that are not explicitly *defined for these routes.
+*/
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,8 +1,10 @@
 const knex = require('../db/connection');
 
+// Function to list reservations for a specific date
 const list = (date) =>
     knex('reservations').select().whereNot('status', 'finished').andWhere('reservation_date', date).orderBy('reservation_time');
 
+// Function to list reservations by mobile number
 const listByMobileNumber = (mobile_number) =>
     knex("reservations")
         .whereRaw(
@@ -10,17 +12,22 @@ const listByMobileNumber = (mobile_number) =>
             `%${mobile_number.replace(/\D/g, "")}%`
         )
         .orderBy("reservation_date");
-        
+
+// Function to read a specific reservation by reservation_id        
 const read = (reservation_id) =>
     knex('reservations').select().where('reservation_id', reservation_id).first();
 
+// Function to create a new reservation
 const create = (newReservation) =>
     knex('reservations').insert(newReservation).returning('*');
+    
+// Function to update a reservation by reservation_id
 
 const update = (reservation_id, updatedReservation) =>
     knex('reservations').where('reservation_id', reservation_id).update(updatedReservation).returning(['first_name', 'last_name', 'mobile_number', 'people', 'reservation_date', 'reservation_time']);
 
-const updateStatus = (reservation_id, status) =>
+// Function to update the status of a reservation by reservation_id
+    const updateStatus = (reservation_id, status) =>
     knex('reservations').where('reservation_id', reservation_id).update({ status: status }).returning('status');
 
 module.exports = {
