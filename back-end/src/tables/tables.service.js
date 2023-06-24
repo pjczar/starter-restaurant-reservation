41e5c59cfc14c7ae7fba71e33b6a89467fe94631
Paul Czarnecki JR <120606767+pjczar@users.noreply.github.com>
2023-06-24 19:33:21 -0300

    fixing failed tests

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -1,25 +1,18 @@
-// Service
-const knex = require('../db/connection');
+const knex = require("../db/connection");
 
-const list = () =>
-    knex('tables').select().orderBy('table_name');
-
-const read = (table_id) =>
-    knex('tables').select().where('table_id', table_id).first();
-
-const create = (newTable) =>
-    knex('tables').insert(newTable).returning('*');
-
-const update = (table_id, reservation_id) =>
-    knex('tables').where('table_id', table_id).update({ reservation_id: reservation_id, occupied: true }).returning('*');
-
-const destroy = (table_id) =>
-    knex('tables').where('table_id', table_id).update({ reservation_id: null, occupied: false }).returning('*');
-
-module.exports = {
-    list,
-    read,
-    create,
-    update,
-    destroy
+function list() {
+  return knex("tables").select("*").orderBy("tables.table_name");
 }
+
+function create(table) {
+  return knex("tables")
+    .insert(table)
+    .returning("*")
+    .then((newTables) => newTables[0]);
+}
+
+function read(table_id) {
+  return knex("tables").select("*").where({ table_id }).first();
+}
+
+module.exports = { list, create, read };
