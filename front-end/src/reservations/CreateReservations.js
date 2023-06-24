2ee5446330c1a588822456ca4891f1bbaff269c1
Paul Czarnecki JR <120606767+pjczar@users.noreply.github.com>
2023-06-24 19:08:38 -0300

    pushed wrong v of reservations fix

--- front-end/src/reservations/CreateReservations.js
+++
@@ -1,5 +0,0 @@
-import ReservationForm from './ReservationForm'
-
-export default function CreateReservations({loadDashboard}) {
-    return (<ReservationForm loadDashboard={loadDashboard} />)
-}
--- front-end/src/reservations/EditReservations.js
+++
@@ -1,5 +0,0 @@
-import ReservationForm from './ReservationForm'
-
-export default function EditReservations({loadDashboard}) {
-    return (<ReservationForm loadDashboard={loadDashboard} />)
-}
