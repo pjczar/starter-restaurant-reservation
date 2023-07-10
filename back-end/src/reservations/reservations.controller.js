35d37fe71cbbe03f5340cb433add111f9700fa72
Paul Czarnecki JR <120606767+pjczar@users.noreply.github.com>
2023-07-10 17:05:18 -0400

    final adjustments

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -162,7 +162,7 @@ function timelineValidator(req, res, next) {
   const minimumReservationTime = new Date(currentTime.getTime() + 60 * 60 * 1000); // Current time + 1 hour
 
   if (reservationDateTime < minimumReservationTime) {
-    return next({ status: 400, message: ` ${reservationDateTime} ${currentTime} ${minimumReservationTime} 'Reservations must be made at least 1 hour in advance.'` });
+    return next({ status: 400, message: "Reservations must be made at least 1 hour in advance." });
   }
 
   next();
